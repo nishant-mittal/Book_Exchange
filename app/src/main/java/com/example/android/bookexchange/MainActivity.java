0447d208c0e565b6e7ed5d6d4a4017aa9b96a3cf
Niranjan <niranjanhari123@gmail.com>
2020-02-28 15:40:33 +0530

    version 1.0

--- app/src/main/java/com/example/android/bookexchange/MainActivity.java
+++ app/src/main/java/com/example/android/bookexchange/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //Might break
                 mBooksList.clear();
+                //
                 //Hello Delete this later
                 for(DataSnapshot snapshot: dataSnapshot.getChildren()) {
                     //Collections.reverse(mBooksList);
